f3e0117e18b9af3b824a9141b3b8d0fae4eadf3f
Mendel Cohen <mmhakohen@gmail.com>
2021-04-23 11:24:59 -0400

    meme crud paths created

--- app/controllers/api/memes_controller.rb
+++ app/controllers/api/memes_controller.rb
@@ -5,4 +5,42 @@ class Api::MemesController < ApplicationController
     render "index.json.jb"
   end
 
+  def show
+    @meme = Meme.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def create
+    @meme = Meme.new(
+      top_text: params[:top_text],
+      img_url: params[:img_url],
+      bottom_text: params[:bottom_text],
+      # user_id: current_user.id
+    )
+    if @meme.save
+      render "show.json.jb"
+    else
+      render json: { errors: @user.errors.full_messages}, status: :bad_request
+    end
+  end
+
+  def update
+    @meme = Meme.find_by(id: params[:id])
+    @meme.top_text = params[:top_text] || @meme.top_text
+    @meme.img_url = params[:img_url] || @meme.img_url
+    @meme.bottom_text = params[:bottom_text] || @meme.bottom_text
+    if @meme.save
+      render "show.json.jb"
+    else
+      render json: { errors: @user.errors.full_messages}, status: :bad_request
+    end
+  end
+
+  def destroy
+    @meme = Meme.find_by(id: params[:id])
+    @meme.delete
+    
+    render json: { message: "MEME DELETED" }
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/memes" => "memes#index"
+    get "/memes/:id" => "memes#show"
+    post "/memes" => "memes#create"
+    patch "/memes/:id" => "memes#update"
+    delete "/memes/:id" => "memes#destroy"
 
     get "/users" => "users#index"
 
